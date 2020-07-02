0a13fbd82f8c7aa2fe59f395797fe28823f8c547
AlvaroCespedes <aecespedes@miuandes.cl>
2020-07-02 13:50:30 -0400

    First commit

---
+++ Examen Alvaro Cespedes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Examen_Alvaro_Cespedes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
